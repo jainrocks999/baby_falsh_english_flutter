f91d10c455eeacf499259214635a71b82916ccfb
Raju Barde <rajubarde587@gmail.com>
2026-08-24 18:26:08 +0530

    italian pushed to play store

--- lib/core/constants/app_language.dart
+++ lib/core/constants/app_language.dart
@@ -79,9 +79,9 @@ class AppLanguageConfig {
       interstitialIos: 'ca-app-pub-6121378252341914/2139406698',
     ),
     AppLanguage.italian: AppAdUnits(
-      bannerAndroid: 'PASTE_ITALIAN_BANNER_ANDROID',
+      bannerAndroid: 'ca-app-pub-6121378252341914~6465236887',
       bannerIos: 'PASTE_ITALIAN_BANNER_IOS',
-      interstitialAndroid: 'PASTE_ITALIAN_INTERSTITIAL_ANDROID',
+      interstitialAndroid: 'ca-app-pub-6121378252341914/3201663775',
       interstitialIos: 'PASTE_ITALIAN_INTERSTITIAL_IOS',
     ),
     AppLanguage.japanese: AppAdUnits(
