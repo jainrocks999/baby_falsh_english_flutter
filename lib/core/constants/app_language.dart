f9321ace3f9124cad52b4726068ac36569528df6
Raju Barde <rajubarde587@gmail.com>
2026-08-26 12:20:02 +0530

    changes

--- lib/core/constants/app_language.dart
+++ lib/core/constants/app_language.dart
@@ -91,7 +91,7 @@ class AppLanguageConfig {
       interstitialIos: 'ca-app-pub-6121378252341914/8391391217',
     ),
     AppLanguage.spanish: AppAdUnits(
-      bannerAndroid: 'ca-app-pub-6121378252341914~4989457973',
+      bannerAndroid: 'ca-app-pub-6121378252341914/5379455730',
       bannerIos: 'ca-app-pub-6121378252341914~6480784411',
       interstitialAndroid: 'ca-app-pub-6121378252341914/3874802378',
       interstitialIos: 'ca-app-pub-6121378252341914/7119078666',
